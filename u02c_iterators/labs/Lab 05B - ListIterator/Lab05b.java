5a7fb0fcad78975e91398abfcb6ef665423e6569
JamesV24 <jverhelst675@gmail.com>
2026-09-24 09:29:33 -0500

    Implement ListIterator test cases in Lab05b
    Added test cases for ListIterator functionality.

--- u02c_iterators/labs/Lab 05B - ListIterator/Lab05b.java
+++ u02c_iterators/labs/Lab 05B - ListIterator/Lab05b.java
@@ -14,5 +14,14 @@ public class Lab05b
 	public static void main ( String[] args )
 	{
 		//add test cases		
+		ListIteratorTest t = new ListIteratorTest("a b c a b c");
+		t.replace("a","+");
+		out.println(t + "\n");
+		t.setTest("a b c d e f g h i j x x x x");
+		t.replace("x","7");
+		out.println(t + "\n");
+		t.setTest("1 2 3 4 5 6 a b c a b c");
+		t.replace.("b","#");
+		out.println(t + "\n");
 	}
 }
